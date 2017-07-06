6aabd81caf89ecfb64251bcf411a25122cb46716
MariaEvstropova <m.evstropova@yandex.ru>
2017-07-06 11:35:09 +0300

    overlay added, srcset

--- script/index.js
+++ script/index.js
@@ -1,23 +1,9 @@
 (function() {
   document.addEventListener("DOMContentLoaded", function() {
-    var galeryItems = document.querySelectorAll(".galery__item");
-    galeryItems.forEach(function(item) {
-      item.addEventListener("click", openImageFullScreen.bind(item));
+    objectFitImages();
+    var galleryItems = document.querySelectorAll(".gallery__item");
+    galleryItems.forEach(function(item) {
+
     })
   });
-
-  function openImageFullScreen() {
-    var image = this.querySelector(".galery__image");
-    launchFullScreen(image);
-  }
-
-  function launchFullScreen(element) {
-    if(element.requestFullScreen) {
-      element.requestFullScreen();
-    } else if(element.mozRequestFullScreen) {
-      element.mozRequestFullScreen();
-    } else if(element.webkitRequestFullScreen) {
-      element.webkitRequestFullScreen();
-    }
-  }
-})()
+})();
